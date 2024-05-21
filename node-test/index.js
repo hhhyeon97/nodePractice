ed595502ff00099d5d893fd2a0bac8d6f5ddb06e
minsol <kth083030@naver.com>
2024-05-21 16:42:03 +0900

    노드실행되는순서연습

---
+++ node-test/index.js
@@ -0,0 +1,13 @@
+//console.log('Hello node!');
+
+console.log('1. 물 가져다 주기');
+
+setTimeout(() => {
+  console.log('2. 햄버거 주기');
+}, 3000);
+
+setTimeout(() => {
+  console.log('3. 감자튀김 주기');
+}, 0);
+
+console.log('4. 콜라 가져다 주기');
