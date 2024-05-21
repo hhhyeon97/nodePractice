ca93ce19e4676b3dfc8480b4472048b0575fb6a3
minsol <kth083030@naver.com>
2024-05-21 20:16:20 +0900

    mongo db crud practice2

--- node-test/practice.js
+++ node-test/practice.js
@@ -4,7 +4,11 @@ const client = new MongoClient(uri);
 
 async function run() {
   const database = client.db('practice');
-  const inventory = database.collection('inventory');
+  //   const inventory = database.collection('inventory');
+  const students = database.collection('students');
+
+  // Q1. insertOne
+
   //   const inventoryData = await inventory.insertOne({
   //     item: 'canvas',
   //     qty: 100,
@@ -12,6 +16,8 @@ async function run() {
   //     size: { h: 28, w: 35.5, uom: 'cm' },
   //   });
   //   console.log('result', inventoryData);
+
+  // Q2. insertMany
 
   //   const inventoryList = [
   //     {
@@ -36,10 +42,13 @@ async function run() {
   //   const inventoryData = await inventory.insertMany(inventoryList);
   //   console.log('result', inventoryData);
 
+  // Q3. find
+
   //   const findItem = await inventory.find({}).toArray();
   //   console.log('result', findItem);
 
   // Q.4
+
   //   const inventoryList = [
   //     {
   //       item: 'journal',
@@ -79,10 +88,12 @@ async function run() {
   //   console.log('result', findItem);
 
   // Q.5
+
   //   const findItem = await inventory.find({ status: 'A', qty: 50 }).toArray();
   //   console.log('result', findItem);
 
-  // Q.6
+  // Q.6 쿼리 $in 사용해보기
+
   //   const findItem = await inventory
   //     .find({
   //       status: { $in: ['A', 'B'] },
@@ -91,7 +102,8 @@ async function run() {
   //   console.log('result', findItem);
   //
 
-  // Q.7
+  // Q.7 $lt
+
   //   const findItem = await inventory
   //     .find({
   //       status: 'A',
@@ -100,7 +112,8 @@ async function run() {
   //     .toArray();
   //   console.log('result', findItem);
 
-  // Q.8
+  // Q.8. $or
+
   //   const findItem = await inventory
   //     .find({
   //       $or: [{ status: 'A' }, { qty: { $lt: 30 } }],
@@ -109,14 +122,62 @@ async function run() {
   //   console.log('result', findItem);
 
   // Q.9 nested field
+
   //   const findItem = await inventory.find({ 'size.uom': 'in' }).toArray();
   //   console.log('result', findItem);
 
   // Q.10
+
   //   const findItem = await inventory.find({ 'size.h': { $gt: 10 } }).toArray();
   //   console.log('result', findItem);
 
-  // Q.11
+  // Q.11 updateOne
+
+  //   const studentList = [
+  //     {
+  //       _id: 1,
+  //       test1: 95,
+  //       test2: 92,
+  //       test3: 90,
+  //       modified: new Date('01/05/2020'),
+  //     },
+  //     {
+  //       _id: 2,
+  //       test1: 98,
+  //       test2: 100,
+  //       test3: 102,
+  //       modified: new Date('01/05/2020'),
+  //     },
+  //     {
+  //       _id: 3,
+  //       test1: 95,
+  //       test2: 110,
+  //       modified: new Date('01/04/2020'),
+  //     },
+  //   ];
+  //   const studentData = await students.insertMany(studentList);
+  //   console.log('result', studentData);
+  //   const updateStudent = await students.updateOne(
+  //     { _id: 3 },
+  //     { $set: { test3: 98 } },
+  //   );
+  //   console.log('result', updateStudent);
+
+  // Q.12 updateMany
+
+  //   const updateAll = await students.updateMany({}, [
+  //     { $set: { test1: 0, status: 'modified' } },
+  //   ]);
+  //   console.log('result', updateAll);
+
+  // Q.13 deleteOne
+  //   const deleteStu = await students.deleteOne({ test2: 92 });
+  //   console.log('result', deleteStu);
+
+  // Q.14 deleteMany
+
+  const deleteAll = await students.deleteMany({ test1: 0 });
+  console.log('result', deleteAll);
 }
 
 run();
